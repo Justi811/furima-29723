aa28e22dd17024b5df06270cb26ad72591201cda
Justi811 <70557501+Justi811@users.noreply.github.com>
2020-10-03 12:03:44 +0900

    テーブルの設定

--- db/migrate/20201003014933_create_items.rb
+++ db/migrate/20201003014933_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
-      t.timestamps
+        t.references :user,             foreign_key:true
+        t.string     :name,             null: false
+        t.text       :description,      null: false
+        t.integer    :category_id,      null: false
+        t.integer    :condition_id,     null: false
+        t.integer    :shipping_fee_id,  null: false
+        t.integer    :shipping_area_id, null: false
+        t.integer    :shipping_date_id, null: false
+        t.integer    :price,            null: false
+        t.timestamps
     end
   end
 end
