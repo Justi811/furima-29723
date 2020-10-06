118a5e795c2c368246df0de31251e7284d445c5f
Justi811 <70557501+Justi811@users.noreply.github.com>
2020-10-06 16:17:49 +0900

    エラーメッセージの表示、バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,23 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :shipping_fee, :shipping_area,:shipping_date
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_date
 
- validates :name , :description, :image, presence: true
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :image, presence: true
 
 
-  with_options presence: true, numericality: { other_than:1} do
-    validetes :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_date_id
+  with_options presence: true, numericality: { greater_than:1} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_date_id
   end
 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
